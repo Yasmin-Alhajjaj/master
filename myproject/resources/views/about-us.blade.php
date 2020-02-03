ba46411801b546da8996f221fd0bf3f1267d2d72
Yasmin-Alhajjaj <yasmin.alhajjaj@gmail.com>
2020-02-03 10:20:00 +0200

    update 18

--- myproject/resources/views/about-us.blade.php
+++ myproject/resources/views/about-us.blade.php
@@ -9,7 +9,7 @@
 
 <h1 >ABOUT US</h1>
 <br><br><br>
-<h3>Waslni is a business platform between customers, commercial activities and even non-commercial Waslniservices, as it provides an extensive network of leading service providers.
+<h3>Waslni is a business platform between customers, commercial activities and even non-commercial (services), as it provides an extensive network of leading service providers.
 It provides knowledge of the best place through the evaluation of previous visitors, as well as knowledge of who provides quality services and reasonable prices.
 It is an easy-to-use platform where it provides detailed information about activities classified according to main categories so that the customer can easily reach the place he wants and know details about him, and the presence of a phone number to communicate and also a map showing his location, in addition to other communication capabilities such as Facebook or its own websites.
 The Waslni platform includes many commercial activities that offer many products such as car sales, salons, and fashion It also includes non-commercial activities that provide services such as
